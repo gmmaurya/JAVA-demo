16261e3a43103d4343188a4b9ff6571c892d8630
gmmaurya <gauravmaurya418@gmail.com>
2023-02-04 01:55:32 +0530

    first commit

--- ram.java
+++ ram.java
@@ -1,7 +1,7 @@
-public class hello{
+public class ram{
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("thi is a git hab of the error of pull the data .");
     }
-
+ 
 }
