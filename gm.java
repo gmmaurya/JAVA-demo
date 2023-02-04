3c806c1fb6d0e0c01c388021730756a70d8329d1
gmmaurya <gauravmaurya418@gmail.com>
2023-02-04 11:50:25 +0530

    gm.java is modified the program

---
+++ gm.java
@@ -0,0 +1,6 @@
+public class gm{
+    public static void main(String[] args) {
+        System.out.println("hello java lover ");
+        System.out.println("theis vs code and use the prform this code of any program");
+    }
+}
